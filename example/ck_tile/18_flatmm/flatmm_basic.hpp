dbc1b668e8ec14e7510267be54376c27b4a0b7e2
Qun Lin <qlin@amd.com>
2025-05-08 10:04:43 +0800

    add init

--- example/ck_tile/18_flatmm/flatmm_basic.hpp
+++ example/ck_tile/18_flatmm/flatmm_basic.hpp
@@ -257,8 +257,8 @@ auto create_args(int argc, char* argv[])
         .insert("repeat", "100", "number of iterations to benchmark the kernel")
         .insert("timer", "gpu", "gpu:gpu timer, cpu:cpu timer")
         .insert("split_k", "1", "splitK value")
-        .insert("cfg_ver", "0", "gemm config version");
-
+        .insert("cfg_ver", "0", "gemm config version")
+        .insert("init", "0", "0:random, 1:linear, 2:constant(1)");
     bool result = arg_parser.parse(argc, argv);
     return std::make_tuple(result, arg_parser);
 }
